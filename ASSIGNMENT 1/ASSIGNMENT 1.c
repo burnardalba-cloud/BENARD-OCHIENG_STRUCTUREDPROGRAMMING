a916a74c84263a8d7c8d0b9f6ab062a918d22a72
burnardalba-cloud <burnardalba@gmail.com>
2025-12-23 17:49:48 +0300

    Add files via upload

---
+++ ASSIGNMENT 1/ASSIGNMENT 1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int a,b,choice;
+    printf("*****************WELCOME*********");
+    printf("To my calculator ");
+    printf("1.Addition\n2,Subtraction\n3.Multiplication\n4.Division\n");
+    printf("What you want to do: ");
+    scanf("%d",&choice);
+    printf("Number 1:");
+    scanf("%d",&a);
+    printf("Number 2:");
+    scanf("%d",&b);
+
+
+    switch(choice)
+    {
+    case 1:
+        printf("%d+%d=%d",a,b,a+b);
+        break;
+
+        case 2:
+        printf("%d-%d=%d",a,b,a-b);
+        break;
+
+        case 3:
+        printf("%d*%d=%d",a,b,a*b);
+        break;
+
+        case 4:
+        printf("%d/%d=%d",a,b,a/b);
+        break;
+
+    }
+
+    return 0;
+
+}
